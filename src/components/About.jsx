2a7f96ad1b992e9df7179b06cda8da8a69c47b22
Nihal K <nihal04x@gmail.com>
2025-08-01 17:18:05 +0000

    update title for clarity and enhance About section content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,25 +25,26 @@ const About = forwardRef((props, ref) => {
 
     return (
         <div className={styles.container} ref={ref}
-        style={{
-            transition: "all 1s ease", 
-            transform: expanded
-                ? "translateY(0px) scale(1)" 
-                : "translateY(-50px) scale(0.9)", 
-            
-            opacity: expanded
-                ? "1" 
-                : "0.5", 
-            
-        }}>
+            style={{
+                transition: "all 1s ease",
+                transform: expanded
+                    ? "translateY(0px) scale(1)"
+                    : "translateY(-50px) scale(0.9)",
+
+                opacity: expanded
+                    ? "1"
+                    : "0.5",
+
+            }}>
             <div className={styles.title}>About Me!</div>
             <p className={styles.about}>
-                Hi, I’m <span>Nihal</span>, a <span>web developer</span> from <span>Kasaragod</span>, <span>Kerala</span>.
-                My passion for technology began over seven years ago when I first started learning <span>HTML</span> and <span>CSS</span>.
-                Since then, I’ve explored various fields like animation, ethical hacking, and app development, but web development became my true focus.
-                I’m currently pursuing a <span>BCA</span> in <span>Software Development</span> and <span>Web Design</span> and have experience working with tools like <span>React</span>, <span>Next.js</span>, and <span>NodeJS</span>.
-                I enjoy building user-friendly <span>projects</span>, combining creativity with functionality to deliver seamless digital experiences.
-                Let’s <span>connect</span> and create something amazing together!
+                Hi, I’m <span>Nihal</span>, a <span>web developer</span> based in <span>Kasaragod</span>, <span>Kerala</span>.
+                My passion for <span>technology</span> began over seven years ago when I first started learning <span>HTML</span> and <span>CSS</span>.
+                Since then I’ve explored animation, ethical hacking, and app development, but web development became my true focus.
+                I recently graduated (2025 MAY) with a <span>BCA</span> in <span>Software Development</span> and <span>Web Design</span>,
+                and I’ve built projects using <span>React</span>, <span>Next.js</span>, and <span>Node.js</span>.
+                I love creating <span>user-friendly</span> experiences that blend <span>creativity</span> with <span>functionality</span>.
+                Let’s connect and build something amazing together!
             </p>
         </div>
     );
