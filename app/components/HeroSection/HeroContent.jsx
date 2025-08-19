aeca37886e97bfa7ca960c6064b1359994828b61
Nihal Local <nihal04x@gmail.com>
2025-08-19 15:18:07 +0530

    fixes

--- app/components/HeroSection/HeroContent.jsx
+++ app/components/HeroSection/HeroContent.jsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from "react"
 
-const titles = ['Frontend Developer', 'Backend ~Developer', 'Humble Hoooooooman', 'Freshhhh ~Graaduat']
+const titles = ['Frontend Developer', 'Backend Developer', 'Humble Hoooooooman', 'Freshhhh Graduate']
 
 export default function HeroContent() {
     const [title, setTitle] = useState("Frontend Developer");
@@ -37,7 +37,7 @@ export default function HeroContent() {
     return (
         <div className="w-full h-full flex flex-col items-center justify-center gap-[0.8em] lg:gap-[1.5em]">
             <h1 className="text-white text-4xl lg:text-7xl font-extrabold">Nihal K</h1>
-            <h2 className="text-md lg:text-3xl text-white/70 font-semibold">I'm a <span ref={titleRef} className="text-indigo-500 inline-block font-mono">{title}</span></h2>
+            <h2 className="text-md lg:text-3xl text-white/70 font-semibold">I'm a <span ref={titleRef} className="text-indigo-500 inline-block font-mono w-[18ch]">{title}</span></h2>
             <p className="opacity-60 text-xs lg:text-xl font-medium max-w-xs lg:max-w-lg text-center">Passionate about building modern, responsive and beautifull web applications</p>
             <div className="flex justify-between gap-[1em] *:py-2 lg:*:py-3 *:px-3 lg:*:px-6  *:text-xs lg:*:text-lg *:font-bold *:rounded-sm lg:*:rounded-lg *:cursor-pointer *:transition-colors *:duration-500 *:ease-in-out">
                 <button className="bg-fuchsia-500 hover:bg-fuchsia-600 shadow-lg/50 shadow-fuchsia-500">View Works</button>
